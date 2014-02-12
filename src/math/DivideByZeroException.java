80f95f92bde7a9043283c1b7e466796463ccd398
nguyengi <nguyengi17@grinnell.edu>
2014-02-12 11:08:31 -0600

    Add comments for custom exception

--- src/math/DivideByZeroException.java
+++ src/math/DivideByZeroException.java
@@ -4,15 +4,30 @@ public class DivideByZeroException
     extends
       Exception
 {
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 1L;
+
+  /*
+   * @param String reason
+   * @purpose calls super constructor of same param
+   */ 
   public DivideByZeroException(String reason){
     super(reason);
-  }
+  } // DivideByZeroException(String)
   
+  /*
+   * @purpose calls super constructor of same param
+   */
   public DivideByZeroException(){
     super();
-  }
+  }// DivideByZeroException()
   
+  /*
+   * @purpose returns customized message
+   */
   public String printException(){
     return "Divide by zero!";
-  }
-}
+  } // printException()
+} // class DivideByZeroException
